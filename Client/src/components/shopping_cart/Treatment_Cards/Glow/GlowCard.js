bee7567260627c816b11b6f5aec4ea0d814c9317
srp-project-it <104577095+srp-project-it@users.noreply.github.com>
2022-05-12 19:17:25 +0530

    Add files via upload

---
+++ Client/src/components/shopping_cart/Treatment_Cards/Glow/GlowCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import ACTION_GLOW_NOT_IN_CART from "../../../../actions/InCart/Treatments/Glow/ACTION_GLOW_NOT_IN_CART";
+import ACTION_DECREMENT_COUNTER from "../../../../actions/Counter/ACTION_DECREMENT_COUNTER";
+import ACTION_SELECTED_DAY_RESET from "../../../../actions/SelectedDay/ACTION_SELECTED_DAY_RESET";
+import ACTION_SELECT_TIME_NOT_ACTIVE from "../../../../actions/SelectTimeActive/ACTION_SELECT_TIME_NOT_ACTIVE";
+import "../../CartCard.css";
+
+const GlowCard = (props) => {
+  const dispatch = useDispatch();
+
+  const handleRemove = () => {
+    dispatch(ACTION_GLOW_NOT_IN_CART());
+    dispatch(ACTION_DECREMENT_COUNTER());
+    dispatch(ACTION_SELECTED_DAY_RESET());
+    dispatch(ACTION_SELECT_TIME_NOT_ACTIVE());
+
+    props.resetAllCartStates();
+  };
+
+  return (
+    <div className="shopping_cart_card_wrapping">
+      <div className="shopping_cart_card_image_circle">
+      <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDyDkLWfMPk7CJNfoHuJhOCaaL0yXjPsPpTA&usqp=CAU" viewBox="0 0 56.356 56.356" width="100%"  />
+      </div>
+      <div className="shopping_cart_card_description">
+        <div className="cart_card_top_container">
+          <h3>Consultation on tools</h3>
+          <p className="shopping_cart_duration">Duration: 50 minutes</p>
+        </div>
+        <div className="cart_card_bottom_container">
+          <div className="shopping_cart_price_container">
+            <p className="shopping_cart_price">Rs 1,000</p>
+          </div>
+          <div className="shopping_cart_remove_button" onClick={handleRemove}>
+            <p>Remove</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GlowCard;
