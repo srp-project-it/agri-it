98220e3951d573a1f5503428fc5e358910f68e02
srp-project-it <104577095+srp-project-it@users.noreply.github.com>
2022-05-13 00:07:27 +0530

    Add files via upload

---
+++ Client/src/components/add_ons/Dermarolling/DermarollingRemovedNotification.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Dermarolling.css";
+import "../../treatments/card_styling.css";
+
+const DermarollingRemovedNotification = (props) => {
+  return (
+    <div className="notification_removed_container">
+      <img src="https://uploads-ssl.webflow.com/61a503785b6ce52e8fbf3b6d/61b87c518515900934480312_Livestock-and-its-Influence-on-Human-Beings.jpeg" viewBox="0 0 56.356 56.356"  width={
+          props.currentScreenSize === ""
+            ? props.initialScreenSize >= 1800
+              ? "20%"
+              : "29%"
+            : props.currentScreenSize >= 1800
+            ? "20%"
+            : "29%"
+        }
+      />
+      <div className="notification_text_container">
+        <h3>Livestock removed</h3>
+      </div>
+    </div>
+  );
+};
+
+export default DermarollingRemovedNotification;
