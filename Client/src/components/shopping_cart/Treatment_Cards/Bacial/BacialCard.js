6bddb0468c2cf6488746a3195d3058a1c137e755
srp-project-it <104577095+srp-project-it@users.noreply.github.com>
2022-05-12 21:39:59 +0530

    Add files via upload

--- Client/src/components/shopping_cart/Treatment_Cards/Bacial/BacialCard.js
+++ Client/src/components/shopping_cart/Treatment_Cards/Bacial/BacialCard.js
@@ -21,38 +21,17 @@ const BacialCard = (props) => {
   return (
     <div className="shopping_cart_card_wrapping">
       <div className="shopping_cart_card_image_circle">
-        <svg width="100%" height="12rem" viewBox="0 0 56 56">
-          <circle cx="28" cy="28" r="22" fill="rgb(241, 241, 241)" />
-          <g
-            id="layer1"
-            fill="none"
-            stroke="#000"
-            transform="translate(10, -174)"
-          >
-            <path
-              fill="rgba(150,221,225, 0.5)"
-              className="bacial_icon_path"
-              d="M6.953 276.362l35.653-.064-17.396-18.032-3.872 6.694-1.533-2.274-2.446 4.323-5.109 4.324-.134-1.921z"
-              strokeWidth=".5"
-            />
-            <path
-              fill="#fff"
-              transform="translate(2, -140)"
-              className="bacial_icon_path"
-              d="M8.136 274.808l3.066-3.203.134 1.217 3.415-2.562.806 1.12 2.205-3.138 1.586 3.01 2.608-4.387.216 5.092 2.742-9.576 1.264 4.676.269-4.004 2.796 6.246.188-3.043 11.938 8.904-15.621-16.462-.296-.289-.054-.032-4.033 7.11-1.56-2.402-2.016 3.876-5.78 4.74-.135-1.89z"
-              strokeWidth=".265"
-            />
-          </g>
-        </svg>
+      <img src="https://www.souriau.com/en-en/system/files/zapp2photo-shutterstock_645059614_-_copie-min.jpg" width="100%" viewBox="0 0 56.356 56.356"/>
+
       </div>
       <div className="shopping_cart_card_description">
         <div className="cart_card_top_container">
-          <h3>Bacial</h3>
+          <h3>Lighting consultation </h3>
           <p className="shopping_cart_duration">Duration: 50 minutes</p>
         </div>
         <div className="cart_card_bottom_container">
           <div className="shopping_cart_price_container">
-            <p className="shopping_cart_price">$120</p>
+            <p className="shopping_cart_price">Rs 1000</p>
           </div>
           <div className="shopping_cart_remove_button" onClick={handleRemove}>
             <p>Remove</p>
